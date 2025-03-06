157b549682c3a4da155c0691cb165f0a47593c4f
jun <mmy519@naver.com>
2025-03-06 17:32:19 +0900

    fix : Validation 적용

--- src/main/java/KYJC/chatApplication/Member/CreateMemberRequest.java
+++ src/main/java/KYJC/chatApplication/Member/CreateMemberRequest.java
@@ -1,6 +1,18 @@
 package KYJC.chatApplication.Member;
 
-public record CreateMemberRequest(String username,
-                                  String loginId,
-                                  String password) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CreateMemberRequest(
+        @Size(min = 1, max = 50) @Pattern(regexp = "^[a-zA-Z가-힣0-9]*$", message = "이름은 알파벳, 한글, 숫자만 가능합니다.")
+        String username,
+
+        @NotBlank @Size(min = 6, max = 20)
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영어와 숫자만 가능합니다. /6자리 이상")
+        String loginId,
+
+        @NotBlank @Size(min = 8, max = 20)
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "소문자, 대문자, 숫자, 특수문자 필수 / 8자리 이상")
+        String password) {
 }
--- src/main/java/KYJC/chatApplication/Member/LoginRequest.java
+++ src/main/java/KYJC/chatApplication/Member/LoginRequest.java
@@ -1,5 +1,16 @@
 package KYJC.chatApplication.Member;
 
-public record LoginRequest(String loginId,
-                           String password) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(
+        @NotBlank @Size(min = 6, max = 20)
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영어와 숫자만 가능합니다. /6자리 이상")
+        String loginId,
+
+        @NotBlank @Size(min = 8, max = 20)
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "소문자, 대문자, 숫자, 특수문자 필수 / 8자리 이상")
+        String password
+) {
 }
