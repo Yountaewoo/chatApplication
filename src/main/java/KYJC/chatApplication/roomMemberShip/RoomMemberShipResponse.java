b3f0a34a6974abf225225eb1ae86fc13faaf2cdf
k800712 <minsung712@yahoo.co.kr>
2025-03-06 16:53:32 +0900

    refactor : simplify RoomMemberShipResponse to use record type

--- src/main/java/KYJC/chatApplication/roomMemberShip/RoomMemberShipResponse.java
+++ src/main/java/KYJC/chatApplication/roomMemberShip/RoomMemberShipResponse.java
@@ -1,17 +1,6 @@
 package KYJC.chatApplication.roomMemberShip;
 
-import lombok.Getter;
 
-@Getter
-public class RoomMemberShipResponse {
-    private final Long id;
-    private final Long userId;
-    private final Long chatRoomId;
-
-    public RoomMemberShipResponse(Long id, Long userId, Long chatRoomId) {
-        this.id = id;
-        this.userId = userId;
-        this.chatRoomId = chatRoomId;
-    }
+public record RoomMemberShipResponse(Long id, Long userId, Long chatRoomId) {
 
 }
